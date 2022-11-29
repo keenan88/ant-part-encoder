0671dff467fa39e18dc291cbeee1c870a1180dd9
Keenan88 <lakingsfan08@gmail.com>
2022-11-29 21:01:09 +0000

    made encoder interface structure to solve magic numbers

--- src/rotary_encoder_interface.c
+++ src/rotary_encoder_interface.c
@@ -14,21 +14,26 @@
 #include <std_msgs/msg/float64_multi_array.h>
 #include "rcCheck.h"
 
-
 #define NUM_ENCODERS 4
 #define TICKS_PER_REV 4096
 
-rotary_encoder_t* front_left_encoder;
-rotary_encoder_t* front_left_encoder;
-rotary_encoder_t* back_left_encoder;
-rotary_encoder_t* back_right_encoder;
+typedef struct {
+    rotary_encoder_t* encoder;
+    int pin_a, pin_b, ID;
+} encoder_interface_t;
+
+encoder_interface_t front_left_encoder_itfc =  {.pin_a = 35, .pin_b = 34, .ID = 1};
+encoder_interface_t back_right_encoder_itfc =  {.pin_a = 39, .pin_b = 36, .ID = 2};
+encoder_interface_t front_right_encoder_itfc = {.pin_a = 23, .pin_b = 22, .ID = 3};
+encoder_interface_t back_left_encoder_itfc =   {.pin_a = 2, .pin_b = 15, .ID = 4};
+
 std_msgs__msg__Float64MultiArray outgoing_wheel_positions;
 rcl_publisher_t motor_joint_state_publisher;
 rcl_timer_t publisher_timer;
 
-rotary_encoder_t* setup_encoder(int pin_A, int pin_B, int encoderID)
+rotary_encoder_t* setup_encoder(encoder_interface_t encoder_interface)
 {
-    rotary_encoder_config_t config = ROTARY_ENCODER_DEFAULT_CONFIG((rotary_encoder_dev_t)encoderID, pin_A, pin_B);
+    rotary_encoder_config_t config = ROTARY_ENCODER_DEFAULT_CONFIG((rotary_encoder_dev_t)encoder_interface.ID, encoder_interface.pin_a, encoder_interface.pin_b);
 
     rotary_encoder_t *encoder = NULL;
     
@@ -70,10 +75,10 @@ double get_encoder_radians(rotary_encoder_t *encoder)
 
 static void publisher_timer_callback(rcl_timer_t * timer, int64_t last_call_time)
 {
-   outgoing_wheel_positions.data.data[0] = get_encoder_radians(front_left_encoder);
-   outgoing_wheel_positions.data.data[1] = get_encoder_radians(back_right_encoder);
-   outgoing_wheel_positions.data.data[2] = get_encoder_radians(front_right_encoder);
-   outgoing_wheel_positions.data.data[3] = get_encoder_radians(back_left_encoder);
+   outgoing_wheel_positions.data.data[0] = get_encoder_radians(front_left_encoder_itfc.encoder);
+   outgoing_wheel_positions.data.data[1] = get_encoder_radians(back_right_encoder_itfc.encoder);
+   outgoing_wheel_positions.data.data[2] = get_encoder_radians(front_right_encoder_itfc.encoder);
+   outgoing_wheel_positions.data.data[3] = get_encoder_radians(back_left_encoder_itfc.encoder);
 
     rcl_publish(
         &motor_joint_state_publisher,
@@ -111,8 +116,8 @@ void init_ros_encoder_interface(rcl_node_t *node_handle, rclc_support_t *support
         )
     );
 
-    front_left_encoder = setup_encoder(35, 34, 1);
-    back_right_encoder = setup_encoder(39, 36, 2);
-    front_right_encoder = setup_encoder(23 ,22, 3);
-    back_left_encoder = setup_encoder(2, 15, 4);
+    front_left_encoder_itfc.encoder =  setup_encoder(front_left_encoder_itfc);
+    back_right_encoder_itfc.encoder =  setup_encoder(back_right_encoder_itfc);
+    front_right_encoder_itfc.encoder = setup_encoder(front_right_encoder_itfc);
+    back_left_encoder_itfc.encoder =   setup_encoder(back_left_encoder_itfc);
 }
