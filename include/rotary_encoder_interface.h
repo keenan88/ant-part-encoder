4ebad9fabfd580d4debd145db46bb54740d50d01
Keenan88 <Lakingsfan08@gmail.com>
2022-11-28 22:01:50 -0500

    stable version from microcontroller code

--- include/rotary_encoder_interface.h
+++ include/rotary_encoder_interface.h
@@ -1,10 +1,22 @@
 #pragma once
 
+#include <unistd.h>
+#include <std_msgs/msg/float64_multi_array.h>
+#include "rotary_encoder.h"
 #include <rcl/rcl.h>
-#include <rcl/error_handling.h>
 #include <rclc/rclc.h>
 #include <rclc/executor.h>
 
-void init_encoders();
+//#include "rotary_encoder.h"
 
-void init_encoders_messaging(rcl_node_t *node_handle, rclc_support_t *support, rclc_executor_t *executor);
+#ifndef M_PI
+#define M_PI 3.14159265358979
+#endif
+
+
+//rotary_encoder_t* setupEncoder(int pinA, int pinB, int encoderID);
+
+int getEncoderValue(rotary_encoder_t *encoder);
+
+void startEncoders(rcl_node_t *node_handle, rclc_support_t *support,
+                              rcl_allocator_t *allocator, rclc_executor_t *executor);
--- src/rotary_encoder_interface.c
+++ src/rotary_encoder_interface.c
@@ -1,95 +1,105 @@
 #include "rotary_encoder_interface.h"
 
-#include <rcl_interfaces/msg/log.h>
-#include <geometry_msgs/msg/twist.h>
-#include <std_msgs/msg/float64_multi_array.h>
+/* PCNT example -- Rotary Encoder
 
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "esp_log.h"
 
-#include "rotary_encoder.h"
+#include <rcl/error_handling.h>
+
+#include <std_msgs/msg/float64_multi_array.h>
 #include "rcCheck.h"
+
 
 #define NUM_MOTORS 4
 
-rotary_encoder_t* fl_encoder;
 rotary_encoder_t* fr_encoder;
+rotary_encoder_t* fl_encoder;
 rotary_encoder_t* bl_encoder;
 rotary_encoder_t* br_encoder;
 std_msgs__msg__Float64MultiArray outgoing_wheel_positions;
 rcl_publisher_t motor_joint_state_publisher;
-rcl_timer_t encoders_publisher_timer;
+rcl_timer_t publisher_timer;
 
-rotary_encoder_t* createEncoder(int pinA, int pinB, int uniqueEncoderID)
+
+static const char *TAG = "example";
+
+rotary_encoder_t* setupEncoder(int pinA, int pinB, int encoderID)
 {
-    // Create rotary encoder instance
-    rotary_encoder_config_t config = ROTARY_ENCODER_DEFAULT_CONFIG((rotary_encoder_dev_t)uniqueEncoderID, pinA, pinB);
+    uint32_t pcnt_unit = encoderID;
+
+    rotary_encoder_config_t config = ROTARY_ENCODER_DEFAULT_CONFIG((rotary_encoder_dev_t)pcnt_unit, pinA, pinB);
     rotary_encoder_t *encoder = NULL;
     ESP_ERROR_CHECK(rotary_encoder_new_ec11(&config, &encoder));
 
     // Filter out glitch (1us)
-    ESP_ERROR_CHECK(encoder -> set_glitch_filter(encoder, 1));
+    ESP_ERROR_CHECK(encoder->set_glitch_filter(encoder, 1));
 
     // Start encoder
-    ESP_ERROR_CHECK(encoder -> start(encoder));
+    ESP_ERROR_CHECK(encoder->start(encoder));
 
     return encoder;
 }
 
-void init_encoders()
+int getEncoderValue(rotary_encoder_t *encoder)
 {
-    fl_encoder = createEncoder(39, 36, 0);
-    fr_encoder = createEncoder(34, 35, 1);
-    bl_encoder = createEncoder(23, 22, 2);
-    br_encoder = createEncoder(15, 2, 3);   
+    return encoder->get_counter_value(encoder);
 }
 
-static void encoder_callback(rcl_timer_t *timer, int64_t last_call_time)
+static void init_motor_pub_msg()
 {
-   outgoing_wheel_positions.data.data[0] = fl_encoder -> get_counter_value(fl_encoder); 
-   outgoing_wheel_positions.data.data[1] = fr_encoder -> get_counter_value(fr_encoder); 
-   outgoing_wheel_positions.data.data[2] = bl_encoder -> get_counter_value(bl_encoder); 
-   outgoing_wheel_positions.data.data[3] = br_encoder -> get_counter_value(br_encoder); 
-    
-    rcl_publish(
-        &motor_joint_state_publisher,
+    outgoing_wheel_positions.data.data = (double *) malloc(NUM_MOTORS*sizeof(double)); // I don't like this, but I have no choice...
+    outgoing_wheel_positions.data.size = NUM_MOTORS;
+    for (int i = 0; i < NUM_MOTORS; ++i)
+    {
+        outgoing_wheel_positions.data.data[i] = 0.0;
+    }
+}
+
+static void publisher_timer_callback(rcl_timer_t * timer, int64_t last_call_time)
+{
+
+   outgoing_wheel_positions.data.data[0] = getEncoderValue(fr_encoder); // get_rad();
+   outgoing_wheel_positions.data.data[1] = getEncoderValue(fl_encoder); // get_rad();
+   outgoing_wheel_positions.data.data[2] = getEncoderValue(bl_encoder); // get_rad();
+   outgoing_wheel_positions.data.data[3] = getEncoderValue(br_encoder); // get_rad();
+
+    rcl_publish(&motor_joint_state_publisher,
         &outgoing_wheel_positions,
         NULL
     );
 }
 
-void init_encoders_messaging(rcl_node_t *node_handle, rclc_support_t *support, rclc_executor_t *executor)
+void startEncoders(rcl_node_t *node_handle, rclc_support_t *support,
+                              rcl_allocator_t *allocator, rclc_executor_t *executor)
 {
-    RCCHECK(
-        rclc_publisher_init_default(
-            &motor_joint_state_publisher,
-            node_handle,
-            ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float64MultiArray),
-            "clearpath_motor_positions"
-        )
-    );
+    init_motor_pub_msg();
 
-    RCCHECK(
-        rclc_timer_init_default(
-            &encoders_publisher_timer,
-            support,
-            RCL_MS_TO_NS(100),
-            encoder_callback
-        )
-    );
+    RCCHECK(rclc_publisher_init_default(
+        &motor_joint_state_publisher,
+        node_handle,
+        ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float64MultiArray),
+        "clearpath_motor_positions"));
 
-    RCCHECK(
-        rclc_executor_add_timer(
-            executor,
-            &encoders_publisher_timer
-        )
-    );
+    RCCHECK(rclc_timer_init_default(
+        &publisher_timer,
+        support,
+        RCL_MS_TO_NS(100),
+        publisher_timer_callback));
 
-    outgoing_wheel_positions.data.data = (double *)malloc(NUM_MOTORS * sizeof(double)); // I don't like this, but I have no choice...
-    outgoing_wheel_positions.data.size = NUM_MOTORS;
-    
-    for (int i = 0; i < NUM_MOTORS; ++i)
-    {
-        outgoing_wheel_positions.data.data[i] = 0.0;
-    }
+    RCCHECK(rclc_executor_add_timer(
+        executor,
+        &publisher_timer));
+
+    fl_encoder = setupEncoder(34, 35, 1);
+    fr_encoder = setupEncoder(39, 36, 2);
+    bl_encoder = setupEncoder(23 ,22, 3);
+    br_encoder = setupEncoder(15, 2, 4);
 }
